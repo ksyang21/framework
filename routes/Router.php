23749a83f0217b974f7a1694ba47dbee1ebc6345
ksyang21 <ksyang1121@hotmail.com>
2023-07-27 21:54:46 +0800

    fix router to controller method

--- routes/Router.php
+++ routes/Router.php
@@ -31,23 +31,14 @@ class Router
 		$this->addRoute($url, $handler, 'GET');
 	}
 
-	private function addRoute(string $url, $handler, string $method, $controller = null, $controllerMethod = null): void
+	private function addRoute(string $url, $handler, string $method): void
 	{
-		// If the controller information is not provided, set the handler as a callback function
-		if (!$controller && !$controllerMethod) {
-			$controller = $handler;
-			$handler    = null;
-		}
-
 		$this->routes[] = [
-			'url'               => $url,
-			'controller'        => $controller,
-			'method'            => $method,
-			'controller_method' => $controllerMethod,
-			'callback'          => $handler,
+			'url'        => $url,
+			'controller' => $handler,
+			'method'     => $method,
 		];
 	}
-
 
 	public function post(string $url, $handler): void
 	{
@@ -61,18 +52,15 @@ class Router
 		foreach ($this->_EXCLUDES as $EXCLUDE) {
 			$request_url = str_replace($EXCLUDE, '', $request_url);
 		}
-		$request_method = $_SERVER['REQUEST_METHOD'];
 
 		foreach ($this->routes as $route) {
 			$pattern    = $this->convertRegex($route['url']);
 			$controller = $route['controller'];
 			$auth       = $this->auth();
-//			$auth = true;
 			if ($auth) {
-				if ($route['method'] === $request_method && preg_match($pattern, $request_url, $matches)) {
-					if (isset($route['controller']) && isset($route['controller_method'])) {
-						// Handle the case where the route has a controller and controller method defined
-						[$controller_class, $method] = [$route['controller'], $route['controller_method']];
+				if ($route['method'] === 'GET' && preg_match($pattern, $request_url, $matches)) {
+					if (is_callable($controller)) {
+						[$controller_class, $method] = $controller;
 						if (class_exists($controller_class)) {
 							$instance = new $controller_class();
 							if (method_exists($instance, $method)) {
@@ -80,10 +68,17 @@ class Router
 								return;
 							}
 						}
-					} else if (isset($route['callback']) && is_callable($route['callback'])) {
-						// Handle the case where the route has a simple callback function
-						call_user_func($route['callback']);
-						return;
+					}
+				} else if ($route['method'] === 'POST' && preg_match($pattern, $request_url, $matches)) {
+					if (is_callable($controller)) {
+						[$controller_class, $method] = $controller;
+						if (class_exists($controller_class)) {
+							$instance = new $controller_class();
+							if (method_exists($instance, $method)) {
+								call_user_func_array([$instance, $method], [$_POST]);
+								return;
+							}
+						}
 					}
 				}
 			}
@@ -101,6 +96,7 @@ class Router
 
 	public function auth(): bool
 	{
+
 		$middlewareInstance = new Middleware();
 
 		foreach ($this->auths as $middleware) {
